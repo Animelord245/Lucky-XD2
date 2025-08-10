f720944818f1b84fd725b282c41a4c0b9d9a0212
Animelord245 <eniobanke245@gmail.com>
2025-08-10 17:55:59 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lucky~XcMXmJpB#0sZlZXW4nd39vKL6sz17lGXN1HxapMpA6EJ72AfV3Nk",
+SESSION_ID: process.env.SESSION_ID || "lucky~ONl2QZQQ#AdMzxMuZEzYHsY73XLiGDLF_GyBd5Bxnkbkb0HeyNeY",
 // add your Session Id make sure it starts with lucky~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -68,7 +68,7 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "2348059389255",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "94777899089",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "Storm",
